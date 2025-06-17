a49db829dd23ee76a75f7814bef64243e028bebf
Your NameChristian <christianghermann@gmail.com>
2025-06-17 09:05:53 -0500

    home/learnmore/gallery pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,26 @@ import { Route, Routes } from "react-router";
 import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+<<<<<<< HEAD
 import GamePage from "./pages/game-page/game-page";
 import Bath from "./pages/game-page/components/bath";
 import Bed from "./pages/game-page/components/bed";
 import Meal from "./pages/game-page/components/meal";
+=======
+import Homepage from "./pages/Homepage";
+import LearnMore from "./pages/LearnMore";
+import Gallery from "./pages/Gallery";
+>>>>>>> 467efe3 (home/learnmore/gallery pages)
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route index element={<p>Home page</p>} />
+        <Route index element={<Homepage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/learn-more" element={<LearnMore />} />
+        <Route path="/gallery" element={<Gallery />} />
       </Route>
       <Route path="/game-page" element={<GamePage />}>
         <Route path="/game-page/bath" element={<Bath />} />
