bb89bc9f545cb7bad24c11e1e0c79d61e6b6dfce
ZeeQ <kw133875@gmail.com>
2025-06-17 21:42:08 -0500

    added some animation styling to homepage

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -4,25 +4,26 @@ import "./Homepage.css";
 export default function Homepage() {
   return (
     // logo for link to homepage
-    <div className="homepage">
-      <main className="homepage-main">
-        <h2 className="homepage-title">Welcome to PixelPal</h2>
-        <p className="homepage-subtitle">Adopt a virtual pet today!</p>
+    <>
+      <div className="homepage">
+        <section className="homepage-main ">
+          <h2 className="homepage-title">Welcome to PixelPal</h2>
+          <p className="homepage-subtitle">Adopt a virtual pet today!</p>
 
-        <div className="pet-placeholder">
-          <img
-            className="homepage-image"
-            src="/images/pertplaceholder.png"
-            alt="Your digital pet"
-          />
-        </div>
-      </main>
-
+          <div className="pet-placeholder">
+            <img
+              className="homepage-image"
+              src="/images/pertplaceholder.png"
+              alt="Your digital pet"
+            />
+          </div>
+        </section>
+      </div>
       <footer className="homepage-footer">
         <NavLink to="/learn-more" className="button-link">
           Learn More
         </NavLink>
       </footer>
-    </div>
+    </>
   );
 }
