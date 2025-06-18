7bd91ccb398585f64b8b1c887349fc25938b697f
Stacy Lee <stacy.lee92@gmail.com>
2025-06-18 10:25:10 -0700

    game-page updates

---
+++ frontend/src/pages/game-page/components/bath.jsx
@@ -0,0 +1,20 @@
+import "/src/pages/game-page/game-page.css";
+
+function Bath() {
+  return (
+    <div>
+      <h1>bath time</h1>
+      <div className="scene-img-container">
+        <div className="scene-img-frame">
+          <img
+            className="scene-img"
+            src="/img/game-play/BathTime.png"
+            alt="bath time img"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Bath;
