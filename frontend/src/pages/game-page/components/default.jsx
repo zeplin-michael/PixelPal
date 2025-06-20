a41b4f68bee98a9f329ffc3bbcf0f8c30e52eb76
Stacy Lee <stacy.lee92@gmail.com>
2025-06-20 09:41:51 -0700

    imported game-page scene components into PalLayout.jsx

---
+++ frontend/src/pages/game-page/components/default.jsx
@@ -0,0 +1,20 @@
+import "/src/pages/game-page/game-page.css";
+
+function Default() {
+  return (
+    <div>
+      <h1>HELLO!</h1>
+      <div className="scene-img-container">
+        <div className="scene-img-frame">
+          <img
+            className="scene-img"
+            src="/img/game-play/alien_idle_360.gif"
+            alt="default idle img"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Default;
