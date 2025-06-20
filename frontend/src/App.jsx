911a83cc727769234c723cdd08752934e098866d
Your NameChristian <christianghermann@gmail.com>
2025-06-20 10:05:38 -0500

    updated app.jsx with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Register from "./auth/Register";
 import Homepage from "./pages/Homepage";
 import LearnMore from "./pages/LearnMore";
 import Gallery from "./pages/Gallery";
+import PalLayout from "./pages/PalLayout";
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/learn-more" element={<LearnMore />} />
         <Route path="/gallery" element={<Gallery />} />
+        <Route path="/pal" element={<PalLayout />} />
       </Route>
     </Routes>
   );
